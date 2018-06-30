ecc318feaa37b180eb0f95255298294e55f43ed8
Shak <web.designs@me.com>
2018-06-30 01:05:50 -0400

    add vue dir

---
+++ JavaScript/React/burger_app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,22 @@
+import React, { Component, Fragment } from 'react';
+import Burger from '../../components/Burger/Burger';
+
+export default class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  }
+  render() {
+    return (
+      <Fragment>
+        <Burger ingredients={this.state.ingredients} />
+
+        <div>Build Controls</div>
+      </Fragment>
+    )
+  }
+}
